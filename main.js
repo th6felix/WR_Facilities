1e591788026061788b0337808732a0c6ce97c881
Emily Crystie <emily_crystie@hotmail.com>
2022-05-04 15:20:01 -0300

    :construction: finalizando nav e menu

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ function onScroll(){
     }
     
 }
+
+function openMenu(){
+    document.body.classList.add('menuExpanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menuExpanded')
+}
