2672566122aba15f6ed726fca337a6a88a68263d
Emily Crystie <emily_crystie@hotmail.com>
2022-05-05 18:32:52 -0300

    :contruction: sections contact e footer

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-function onScroll(){
+onScroll()
+window.addEventListener('scroll', onScroll)
+
+//Gerenciamento de Scroll
+function showNavOnScroll(){
     if (scrollY > 0){
         navigation.classList.add('scroll')
     } else {
         navigation.classList.remove('scroll')
     }
-    
+}
+
+function showToBackTopButtonOnScroll(){
+    if (scrollY > 400){
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+function onScroll(){
+    showNavOnScroll()
+    showToBackTopButtonOnScroll()
 }
 
 
-function openMenu(){
-    document.body.classList.add('menuExpanded')
-}
-
-function closeMenu(){
-    document.body.classList.remove('menuExpanded')
-}
-
-
+//Aparição dos elementos conforme scroll
 ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -30,5 +38,18 @@ ScrollReveal({
     #services .card,
     #about,
     #about header,
-    #about .content
+    #about .content,
+    #contact,
+    #contact header,
+    #contact content
 `);
+
+
+//Funcionamento do menu
+function openMenu(){
+    document.body.classList.add('menuExpanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menuExpanded')
+}
