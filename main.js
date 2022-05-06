325f6c78db3f143fe4fe96cd10784ec1e266d68e
Emily Crystie <emily_crystie@hotmail.com>
2022-05-06 17:54:18 -0300

    📱terminando responsividade desktop

--- main.js
+++ main.js
@@ -1,6 +1,44 @@
 onScroll()
 window.addEventListener('scroll', onScroll)
 
+
+function onScroll(){
+    showNavOnScroll()
+    showBackTopButtonOnScroll()
+
+    // activateMenuAtCurrentSection(home)
+    // activateMenuAtCurrentSection(services)
+    // activateMenuAtCurrentSection(about)
+    // activateMenuAtCurrentSection(contact)
+}
+
+// function activateMenuAtCurrentSection(section){
+//       const targetLine = scrollY + innerHeight / 2
+
+//   // verificar se a seção passou da linha
+//   // quais dados vou precisar?
+//   const sectionTop = section.offsetTop
+//   const sectionHeight = section.offsetHeight
+//   const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+//   // verificar se a base está abaixo da linha alvo
+
+//   const sectionEndsAt = sectionTop + sectionHeight
+//   const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+//   // limites da seção
+//   const sectionBoundaries =
+//     sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+//   const sectionId = section.getAttribute('id')
+//   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+//   menuElement.classList.remove('active')
+//   if (sectionBoundaries) {
+//     menuElement.classList.add('active')
+//   }
+// }
+
 //Gerenciamento de Scroll
 function showNavOnScroll(){
     if (scrollY > 0){
@@ -10,19 +48,13 @@ function showNavOnScroll(){
     }
 }
 
-function showToBackTopButtonOnScroll(){
+function showBackTopButtonOnScroll(){
     if (scrollY > 400){
         backToTopButton.classList.add('show')
     } else {
         backToTopButton.classList.remove('show')
     }
 }
-
-function onScroll(){
-    showNavOnScroll()
-    showToBackTopButtonOnScroll()
-}
-
 
 //Aparição dos elementos conforme scroll
 ScrollReveal({
@@ -43,7 +75,6 @@ ScrollReveal({
     #contact header,
     #contact content
 `);
-
 
 //Funcionamento do menu
 function openMenu(){
